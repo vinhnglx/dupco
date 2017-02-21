0a6bc22c1a90ab5bb494faf1cce233307f9d0501
Vincent Nguyen <vinh.nglx@gmail.com>
2017-02-22 00:47:55 +0800

    Pass test initialize new Spider

--- lib/dupco/spider.rb
+++ lib/dupco/spider.rb
@@ -1,2 +1,8 @@
 class Spider
+  attr_reader :url
+
+  def initialize(url)
+    @url = url
+  end
 end
+
--- test/test_spider.rb
+++ test/test_spider.rb
@@ -2,6 +2,7 @@ require 'helper'
 
 class SpiderTest < MiniTest::Test
   def test_init_spider
+    url = "test/fixture.html"
     spider = Spider.new(url)
 
     assert_instance_of Spider, spider
