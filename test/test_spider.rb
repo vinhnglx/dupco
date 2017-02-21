b4622c68985a0ecc17b0584797552ce28f63e77d
Vincent Nguyen <vinh.nglx@gmail.com>
2017-02-22 00:46:12 +0800

    Add test crawl for Spider

--- test/test_spider.rb
+++ test/test_spider.rb
@@ -2,9 +2,18 @@ require 'helper'
 
 class SpiderTest < MiniTest::Test
   def test_init_spider
-    spider = Spider.new
+    spider = Spider.new(url)
 
     assert_instance_of Spider, spider
+  end
+
+  def test_crawl
+    url = "test/fixture.html"
+    spider = Spider.new(url)
+    body = spider.crawl(class: "thumbnail col-inline")
+
+    assert_instance_of Nokogiri::XML::NodeSet, body
+    assert_equal(2, body.count)
   end
 
 end
