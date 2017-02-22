589671b0db1787a0c39396b563fd98778b5e6d05
Vincent Nguyen <vinh.nglx@gmail.com>
2017-02-22 09:21:39 +0800

    Refactor SpiderTest

--- test/test_spider.rb
+++ test/test_spider.rb
@@ -1,21 +1,20 @@
 require 'helper'
 
 class SpiderTest < MiniTest::Test
-  def test_init_spider
+  def setup
     url = "test/fixture.html"
-    spider = Spider.new(url)
+    @spider = Spider.new(url)
+  end
 
-    assert_instance_of Spider, spider
+  def test_init_spider
+    assert_instance_of Spider, @spider
   end
 
   def test_crawl
-    url = "test/fixture.html"
-    spider = Spider.new(url)
-
-    body = spider.crawl(class: "a")
+    body = @spider.crawl(class: "a")
     assert_equal(1, body.count)
 
-    body_1 = spider.crawl(class: "thumbnail col-inline")
+    body_1 = @spider.crawl(class: "thumbnail col-inline")
     assert_equal(2, body_1.count)
   end
 end
