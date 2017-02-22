1936904d962c8cc8b7bcf87f5090ab35b36fdfcf
Vincent Nguyen <vinh.nglx@gmail.com>
2017-02-23 02:19:14 +0800

    Add comment for VikiRaw class

--- lib/dupco/viki_raw.rb
+++ lib/dupco/viki_raw.rb
@@ -1,4 +1,19 @@
 class VikiRaw
+  # Public: Parsing to get contents from Viki.com's home page
+  #
+  # Parameter
+  #
+  #   raw - Array of Html element after crawl by Spider
+  #
+  # Example
+  #
+  #   spider = Spider.new('https://www.viki.com')
+  #   body = spider.crawl(class: '.abc')
+  #   viki = Raw.new(body, VikiRaw.new)
+  #   viki.process
+  #   # => ["{'title': '', 'image_src': '', 'url': ''}, {..}"]
+  #
+  # Returns Array of hash contents
   def process(raw)
     raw.body.map {|element|
       {
